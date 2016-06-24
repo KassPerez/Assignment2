b1c8d5ba9dee479106666c5d0ec7f525fbb842d9
kassperez <kassandra.perez@utexas.edu>
2016-06-24 11:44:46 -0500

    fixed start message

--- Game_Functions.java
+++ Game_Functions.java
@@ -16,11 +16,21 @@ public class Game_Functions {
 	
 	public static void startMessage()
 	{
-		System.out.println("Welcome to Mastermind. Here are the rules. \n\nThis is a text version of the classic board game Mastermind."
+		System.out.print("Welcome to Mastermind. Here are the rules. \n\nThis is a text version of the classic board game Mastermind."
 				+ "The computer will think of a secret code. The code consists of " + GameConfiguration.pegNumber 
 				+ " colored pegs. \nThe pegs MUST be one of " + GameConfiguration.colors.length
-				+ "colors: " + Arrays.toString(GameConfiguration.colors)
-				+ " A color may appear more than once in the code. You try to guess what "
+				+ " colors: ");
+		for(int i = 0;i < GameConfiguration.colors.length;i++)
+		{
+			System.out.print(GameConfiguration.colors[i]);
+			if(i == GameConfiguration.colors.length - 1)
+			{
+				System.out.print(".");
+			}
+			else
+				System.out.print(", ");
+		}
+		System.out.print(" A color may appear more than once in the code. You try to guess what "
 				+ "colored pegs are in the code and what order they are in. \nAfter you make a valid guess the result (feedback) will be displayed."
 				+ "\nThe result consists of a black peg for each peg you have guessed exactly correct (color and position) in your guess. \nFor each peg in "
 				+ "the guess that is the correct color, but is out of position, you get a white peg. For each peg that is fully incorrect,"
@@ -123,7 +133,7 @@ public class Game_Functions {
 	
 	public static void History(int guesses){
 	for(int i=0;i<guesses;i+=1){
-			System.out.println("Guess "+ (i+1) + " : " + History[i] + "		" + History[i] + " -> Result: " + Black_Peg_H[i] + " black peg(s) and " + White_Peg_H[i] + " white peg(s)");
+			System.out.println("Guess "+ (i+1) + ": " + History[i] + "		" + History[i] + " -> Result: " + Black_Peg_H[i] + " black peg(s) and " + White_Peg_H[i] + " white peg(s)");
 		}
 	}
 }
